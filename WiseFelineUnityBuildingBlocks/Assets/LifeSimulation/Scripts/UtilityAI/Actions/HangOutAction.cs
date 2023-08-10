6c567f2f21ac9f8759f072f390dd1c91de1b3d66
Arya Valizadeh <arya.valizadeh@outlook.com>
2023-08-10 13:25:05 +0330

    update scene

---
+++ WiseFelineUnityBuildingBlocks/Assets/LifeSimulation/Scripts/UtilityAI/Actions/HangOutAction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NoOpArmy.WiseFeline;
+using NoOpArmy.WiseFeline.Sample;
+using UnityEngine.UIElements;
+
+public class HangOutAction : ActionBase
+{
+    protected override void OnInitialized()
+    {
+
+    }
+
+    protected override void OnStart()
+    {
+
+    }
+    protected override void OnUpdate()
+    {
+
+    }
+    protected override void UpdateTargets() 
+    {
+
+    }
+
+}
